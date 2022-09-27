73ea80670b8a96d67bded7781d8fbcbf8ab2b7cc
Jonathan <ghuyers98@gmail.com>
2022-09-27 15:52:22 +0800

    add dark mode

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// navbar
 const primaryNav = document.querySelector(".primary-navigation");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 
@@ -13,12 +14,24 @@ navToggle.addEventListener("click", () => {
   }
 });
 
+// animation
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     entry.target.classList.toggle("show", entry.isIntersecting);
-    if (entry.isIntersecting) observer.unobserve(entry.target);
+    // if (entry.isIntersecting) observer.unobserve(entry.target);
   });
 });
 
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
+
+// dark mode
+const darkModeToggle = document.querySelector(".dark-mode-toggle");
+const sunIcon = document.querySelector(".sun-icon");
+const moonIcon = document.querySelector(".moon-icon");
+
+darkModeToggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+  sunIcon.classList.toggle("sun-icon-darkmode");
+  moonIcon.classList.toggle("moon-icon-darkmode");
+})
